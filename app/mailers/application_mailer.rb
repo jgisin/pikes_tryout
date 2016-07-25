d1c2c16f5c4dfd5e2bc116f56449c76056900b25
Jeffrey Gisin <jgisin@gmail.com>
2016-07-24 18:29:41 -0600

    heroku

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -9,6 +9,6 @@ class ApplicationMailer < ActionMailer::Base
 
   def notify_admin(player)
     @player = player
-    mail(to: ENV['ADMIN_EMAIL'], subject: "#{@player.name} has registered to tryout")
+    mail(to: ENV['ADMIN_EMAIL'], subject: "#{@player.name} has registered to tryout.")
   end
 end
