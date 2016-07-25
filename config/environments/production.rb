2dc78293bee44370572af789e51af2df2cc5cc94
Jeffrey Gisin <jgisin@gmail.com>
2016-07-24 18:31:29 -0600

    heroku email config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -94,7 +94,7 @@ Rails.application.configure do
     :enable_starttls_auto => true
   }
 
-  config.action_mailer.deliver_method ||= :smtp
+  config.action_mailer.delivery_method ||= :smtp
 
   config.action_mailer.default_url_options = {
     :host => 'www.vigilantetryout.com'
